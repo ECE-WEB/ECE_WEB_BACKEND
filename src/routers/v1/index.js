bc31d13618de39a6c40d9415f5452773e45184c9
Dinesh <kdineshkumar8877@gmail.com>
2025-03-19 07:11:16 +0530

    Announcement

--- src/routers/v1/index.js
+++ src/routers/v1/index.js
@@ -1,4 +1,24 @@
 const router = require('express').Router()
 const server = require('./serverrouter')
+
+const announcementController = require("../../controllers/index");
+
 router.use('/server',server)
+
+
+router.route("/")
+    .get(announcementController.allAnnouncements)
+    .post(announcementController.createAnnouncement)
+
+router.route("/new")
+    .get(announcementController.newAnnouncement)
+
+router.route("/:id/edit")
+    .get(announcementController.editAnnouncement)
+
+router.route("/:id")
+    // .get(announcementController.showAnnouncement)
+    .put(announcementController.updateAnnouncement)
+    .delete(announcementController.deleteAnnouncement)
+
 module.exports=router
